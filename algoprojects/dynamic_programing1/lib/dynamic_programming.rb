28f16a3cf86bae67881a1e94c4a4ca87f37798fb
Rebekah Liu <liu.rebekah@gmail.com>
2017-10-25 10:20:03 -0700

    add dynamic programming

---
+++ algoprojects/dynamic_programing1/lib/dynamic_programming.rb
@@ -0,0 +1,127 @@
+require 'byebug'
+
+class DynamicProgramming
+
+  def initialize
+    @blair_cache = { 1 => 1, 2 => 2 }
+    @frog_cache = {
+      1 => [[1]],
+      2 => [[2], [1,1]],
+      3 => [[1,1,1], [2,1], [1,2], [3]]
+    }
+
+  end
+
+  def blair_nums(num)
+    #check cache
+    return @blair_cache[num] unless @blair_cache[num].nil?
+    # If not found, 1) perform the recursion, 2) store the answer, 3) return the answer.
+    ans = blair_nums(num-1) + blair_nums(num-2) + 2*(num-1) - 1
+    @blair_cache[num] = ans
+    return ans
+  end
+
+  def frog_hops_bottom_up(n)
+    hops = frog_cache_builder(n)
+    hops[n]
+  end
+
+  def frog_cache_builder(num)
+    cache = [[], [[1]], [[2], [1,1]], [[1,1,1], [2,1], [1,2], [3]]]
+    return cache if num < 4
+
+    (4..num).to_a.each do |i|
+      total_hops = []
+
+      cache[i - 1].each do |hops|
+        total_hops << hops + cache[1][0]
+      end
+
+      cache[i - 2].each do |hops|
+          total_hops << hops + cache[2][0]
+          total_hops << hops + cache[2][1]
+      end
+
+      cache[i - 3].each do |hops|
+          total_hops << hops + cache[3][0]
+          total_hops << hops + cache[3][1]
+          total_hops << hops + cache[3][2]
+          total_hops << hops + cache[3][3]
+      end
+      cache[i] = total_hops.uniq
+    end
+    return cache
+  end
+
+  def frog_hops_top_down(n)
+    return @frog_cache[n] if @frog_cache[n]
+
+    frog_hops_top_down_helper(n)
+
+    @frog_cache[n]
+  end
+
+  def frog_hops_top_down_helper(n)
+    results = []
+    results += frog_hops_top_down(n - 1).map do |hop|
+      hop + [1]
+    end
+    results += frog_hops_top_down(n - 2).map do |hop|
+      hop + [2]
+    end
+    results += frog_hops_top_down(n - 3).map do |hop|
+      hop + [3]
+    end
+
+    @frog_cache[n] = results
+  end
+
+  def super_frog_hops(n, k)
+    super_frog_cache = super_frog_cache_builder(n, k)
+
+    super_frog_cache[k][n]
+  end
+
+  def super_frog_cache_builder(n, k)
+    cache = {}
+
+    (1..n).each do |col|
+      (1..k).each do |row|
+
+        cache[row] = [[[]]] unless cache[row]
+        result = []
+        if row == 1
+          result << cache[row][col - 1][0] + [1]
+        elsif row == col
+          result = cache[row - 1][col].dup
+          result << [row]
+        elsif col > row
+          (1...col).each do |column|
+            cache[row][column].each do |hop|
+              next if (col - column) > k
+              result << hop + [col - column]
+            end
+          end
+        elsif row > col
+          result = cache[col][col]
+        end
+
+        cache[row][col] = result
+      end
+    end
+
+    cache
+  end
+
+  def knapsack(weights, values, capacity)
+
+  end
+
+  # Helper method for bottom-up implementation
+  def knapsack_table(weights, values, capacity)
+
+  end
+
+  def maze_solver(maze, start_pos, end_pos)
+  end
+end
